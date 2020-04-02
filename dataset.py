0e678f95bb2684478cddff5ebe3a0b8c99fc13cc
gagiter <gagiter@163.com>
2020-04-02 16:38:19 +0800

    train.

--- dataset.py
+++ dataset.py
@@ -1,5 +1,9 @@
 
+import os
 from torch.utils.data import Dataset
+from PIL import Image
+from torchvision.transforms import RandomCrop
+import torchvision.transforms.functional as TF
 
 
 class Vaihgen(Dataset):
@@ -10,4 +14,62 @@ class Vaihgen(Dataset):
         return len(self.samples)
 
     def __getitem__(self, idx):
-        return self.samples[idx]
+        return [self.samples[idx], self.samples[idx]]
+
+
+class Data(Dataset):
+    def __init__(self, data_dir, mode='train'):
+        self.file_triples = []
+        self.data_dir = data_dir
+        self.mode = mode
+        self.read_triples()
+
+    def read_triples(self):
+        triple_file = None
+        if self.mode == 'train':
+            triple_file = 'train.txt'
+        elif self.mode == 'val':
+            triple_file = 'val.txt'
+        if triple_file is None:
+            raise ValueError("The mode only surport train or val")
+
+        triple_file = os.path.join(self.data_dir, triple_file)
+        with open(triple_file, 'r') as file:
+            line = file.readline()
+            while line:
+                line = line.strip()
+                if not line.startswith('#'):
+                    triple = line.split()
+                    self.file_triples.append((triple[0], triple[1], triple[2]))
+                line = file.readline()
+
+    def __len__(self):
+        return len(self.file_triples)
+
+    def __getitem__(self, idx):
+
+        triple = self.file_triples[idx]
+        color_file = os.path.join(self.data_dir, 'color', triple[0])
+        label_file = os.path.join(self.data_dir, 'label', triple[1])
+        depth_file = os.path.join(self.data_dir, 'height', triple[2])
+
+        color_image = Image.open(color_file)
+        label_image = Image.open(label_file)
+        depth_image = Image.open(depth_file)
+        depth_image = TF.to_grayscale(depth_image)
+
+        color_image = TF.resize(color_image, (512, 512))
+        label_image = TF.resize(label_image, (512, 512))
+        depth_image = TF.resize(depth_image, (512, 512))
+
+        # crop_paras = RandomCrop.get_params(color_image, output_size=(512, 512))
+        # color_image = TF.crop(color_image, *crop_paras)
+        # label_image = TF.crop(label_image, *crop_paras)
+        # depth_image = TF.crop(depth_image, *crop_paras)
+
+        color_image = TF.to_tensor(color_image)
+        label_image = TF.to_tensor(label_image)
+        depth_image = TF.to_tensor(depth_image)
+
+        return (color_image, label_image, depth_image)
+
--- train.py
+++ train.py
@@ -8,72 +8,81 @@ from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from model import Net
+import dataset
+import segmentation_models_pytorch as smp
+from torch.utils.tensorboard import SummaryWriter
 
 
 parser = argparse.ArgumentParser(description='PyTorch implementation of CombinedHeight')
-parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+
+parser.add_argument('--batch-size', type=int, default=2, metavar='N',
                     help='input batch size for training (default: 64)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
+parser.add_argument('--data_dir', type=str, default='data/Postdam',
+                    help='where to load data')
+parser.add_argument('--resume', action='store_true', default=True,
+                    help='resume training from checkpoint')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--gpu_id', type=int, default=0, metavar='N',
+                    help='which gpu want to use')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
 parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                     help='SGD momentum (default: 0.5)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='disables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--save-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before checkpointing')
-parser.add_argument('--resume', action='store_true', default=False,
-                    help='resume training from checkpoint')
+parser.add_argument('--epoch_start', type=int, default=0, metavar='N',
+                    help='number of start epoch')
+parser.add_argument('--epoch_end', type=int, default=1000, metavar='N',
+                    help='number of end epoch')
+
 args = parser.parse_args()
 
 use_cuda = torch.cuda.is_available() and not args.no_cuda
-device = torch.device('cuda' if use_cuda else 'cpu')
-torch.manual_seed(args.seed)
-if use_cuda:
-    torch.cuda.manual_seed(args.seed)
+device = torch.device('cuda:%d' % args.gpu_id if use_cuda else 'cpu')
 
-data_path = os.path.join(os.path.expanduser('~'), '.torch', 'datasets', 'mnist')
-train_data = datasets.MNIST(data_path, train=True, download=True,
-                            transform=transforms.Compose([
-                              transforms.ToTensor(),
-                              transforms.Normalize((0.1307,), (0.3081,))]))
-test_data = datasets.MNIST(data_path, train=False, transform=transforms.Compose([
-                             transforms.ToTensor(),
-                             transforms.Normalize((0.1307,), (0.3081,))]))
+train_data = dataset.Data(args.data_dir, 'train')
+val_data = dataset.Data(args.data_dir, 'val')
 
-train_loader = DataLoader(train_data, batch_size=args.batch_size,
-                          shuffle=True, num_workers=4, pin_memory=True)
-test_loader = DataLoader(test_data, batch_size=args.batch_size,
-                         num_workers=4, pin_memory=True)
+train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
+val_loader = DataLoader(val_data, batch_size=args.batch_size)
 
 
-model = Net().to(device)
+model = smp.Unet('resnet34', encoder_weights=None, activation='sigmoid')
+model = model.to(device)
 optimiser = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+writer = SummaryWriter()
 
 if args.resume:
-    model.load_state_dict(torch.load('model.pth'))
-    optimiser.load_state_dict(torch.load('optimiser.pth'))
+    model.load_state_dict(torch.load('checkpoint/model.pth'))
+    optimiser.load_state_dict(torch.load('checkpoint/optimiser.pth'))
+    # args.epoch_start = torch.load('checkpoint/optimiser.pth')['epoch']
 
-model.train()
-train_losses = []
+for epoch in range(args.epoch_start, args.epoch_end):
+    model.train()
+    train_losses = []
+    for data in train_loader:
+        color = data[0].to(device=device)
+        depth = data[2].to(device=device)
+        optimiser.zero_grad()
+        output = model(color)
+        loss = F.l1_loss(output, depth)
+        loss.backward()
+        train_losses.append(loss.item())
+        optimiser.step()
 
-for i, (data, target) in enumerate(train_loader):
-    data = data.to(device=device, non_blocking=True)
-    target = target.to(device=device, non_blocking=True)
-    optimiser.zero_grad()
-    output = model(data)
-    loss = F.nll_loss(output, target)
-    loss.backward()
-    train_losses.append(loss.item())
-    optimiser.step()
+    if epoch % 1 == 0:
+        loss = sum(train_losses) / len(train_losses)
+        print(epoch, loss)
+        writer.add_scalar('loss', loss, global_step=epoch)
+        writer.add_images('color', color, global_step=epoch)
+        writer.add_images('depth', depth, global_step=epoch)
+        writer.add_images('predict', output, global_step=epoch)
 
-    if i % 10 == 0:
-        print(i, loss.item())
-        torch.save(model.state_dict(), 'model.pth')
-        torch.save(optimiser.state_dict(), 'optimiser.pth')
-        torch.save(train_losses, 'train_losses.pth')
+        torch.save(model.state_dict(), 'checkpoint/model.pth')
+        torch.save(optimiser.state_dict(), 'checkpoint/optimiser.pth')
+        torch.save({'epoch': epoch}, 'checkpoint/epoch.pth')
+        # torch.save(train_losses, 'train_losses.pth')
+
+writer.close()
+
 
 
